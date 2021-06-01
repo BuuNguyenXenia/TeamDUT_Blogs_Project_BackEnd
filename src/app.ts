4db9749e2eee029a89b4c0e5fc2671075121501a
HuyHoXenia <huy.ho@xenia.tech>
2021-06-01 16:19:04 +0700

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -3,20 +3,22 @@ import config from "config";
 import log from "./logger";
 import connect from "./db/connect";
 import routes from "./routes";
-import {deserializeUser} from "./middleware"
+import { deserializeUser } from "./middleware";
+import cors from "cors";
 
 const port = config.get("port") as number;
 const host = config.get("host") as string;
 
 const app = express();
-app.use(deserializeUser)
+app.use(cors());
+app.use(deserializeUser);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, host, () => {
-    log.info(`Server listing at http://${host}:${port}`);
-    connect();
+  log.info(`Server listing at http://${host}:${port}`);
+  connect();
 
-    routes(app);
+  routes(app);
 });
